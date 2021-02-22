81acad5006023e8c8fe1d2b12d9655f69ad90f1a
Visar Ibishi <visar.ibishi@openresearch.com>
2021-02-22 16:18:15 +0100

    Fixed swiftLint warnings

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "Fusion", url: "https://nexus.i.openresearch.com/repository/raw/fusion/Fusion-1.0.0.xcframework.zip", checksum: "147248b48fac2da55c9e8533471a6bc2af22732badcfcf7f350b7bed12e276b9")
+        .binaryTarget(
+            name: "Fusion",
+            url: "https://nexus.i.openresearch.com/repository/raw/fusion/Fusion-1.0.0.xcframework.zip",
+            checksum: "147248b48fac2da55c9e8533471a6bc2af22732badcfcf7f350b7bed12e276b9")
     ]
 )
